334587899802ee29613758a0cac6b4ba2fbeefe0
yinwoods <yinwoods@163.com>
2016-01-31 16:07:56 +0800

    remove unuseful files

--- .device2.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yinwoods/bs/device2.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/yinwoods/bs/device2.ko /home/yinwoods/bs/device2.o /home/yinwoods/bs/device2.mod.o
--- .driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yinwoods/bs/driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/yinwoods/bs/driver.ko /home/yinwoods/bs/driver.o /home/yinwoods/bs/driver.mod.o
