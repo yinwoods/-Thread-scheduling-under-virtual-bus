f4ffc2acf764e62b7fa7eb9c77d4099d0a4c4367
yinwoods <yinwoods@163.com>
2016-02-15 11:36:50 +0800

    动态创建多个线程

--- .drivers.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yinwoods/bs/drivers.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/yinwoods/bs/drivers.ko /home/yinwoods/bs/drivers.o /home/yinwoods/bs/drivers.mod.o
